17c58721ffd8df766cf4b1ffdb011e172324e52a
Bruno <bj_mal@hotmail.com>
2023-11-02 11:00:24 -0300

    Colocando pices no tabuleiro

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,16 +1,22 @@
 package boardgame;
 
+// Classe Position da camada de BoardGame que representa a posição no tabuleiro//
+
 public class Position {
 
 	private int linha;
 	private int coluna;
 
+	// Criação dos construtores//
+	
 	public Position(int linha, int coluna) {
 
 		this.linha = linha;
 		this.coluna = coluna;
 	}
 
+	// Criação dos Getter e Setters//
+	
 	public int getLinha() {
 		return linha;
 	}
@@ -27,6 +33,8 @@ public class Position {
 		this.coluna = coluna;
 	}
 
+	// Criação do ToString para imprimir a posição na tela//
+	
 	@Override
 	public String toString() {
 		return linha + " , " + coluna;
